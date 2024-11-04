8870a81551efae2e92d7af87451cf7a676c69ab1
Shaahid <shaahid2453@gmail.com>
2024-11-04 08:54:00 -0800

    blob design improved

--- src/app/speech.tsx
+++ src/app/speech.tsx
@@ -53,7 +53,7 @@ const Speech: React.FC<SpeechProps> = ({ response }) => {
 
       initializeAudio();
     }
-    
+
     return () => {
       if (audioUrl) {
         URL.revokeObjectURL(audioUrl);
@@ -63,16 +63,32 @@ const Speech: React.FC<SpeechProps> = ({ response }) => {
 
   return (
     <div className="flex flex-col items-center">
-     
-      <div className='w-[150px] h-[150px] rounded-full object-cover text-center text-white shadow-2xl bg-yellow-500 shadow-yellow-700/60 hover:shadow-yelow-600/70 transition-shadow duration-300 mb-4 flex justify-center items-center'>
+
+      {/* <div className='w-[150px] h-[150px] rounded-full object-cover text-center text-white shadow-2xl bg-yellow-500 shadow-yellow-700/60 hover:shadow-yelow-600/70 transition-shadow duration-300 mb-4 flex justify-center items-center'>
       {isLoading ? <span className="loading loading-dots loading-lg"></span> : <div>shaah1d</div> }
      </div>
-   
-      <audio 
-        ref={audioRef} 
-        style={{ display: 'none' }} 
+    */}
+ 
+    <div className="relative w-[200px] h-[200px] overflow-hidden rounded-full shadow-2xl bg-yellow-500 shadow-yellow-700/60 hover:shadow-yelow-600/70">
+      <div className="absolute inset-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-yellow-300 blur-xl" />
+      <div 
+        className="absolute inset-0 flex items-center justify-center"
+        style={{
+          background: 'radial-gradient(circle at center, rgb(239 68 68) 0%, rgb(249 115 22) 25%, rgb(251 146 60) 50%, rgb(234 179 8) 75%, rgb(254 240 138) 90%, rgb(255 255 255) 100%)'
+        }}
+      >
+        <p className="text-white  z-10 text-center">
+      {isLoading ? <span className="loading loading-dots loading-lg"></span> : <></>}
+        
+        </p>
+      </div>
+    </div>
+
+      <audio
+        ref={audioRef}
+        style={{ display: 'none' }}
       />
-    
+
     </div>
   );
 };
